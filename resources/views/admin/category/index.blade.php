b26aeaa1da9e98bfddb6d6a759f5418fe1172718
Mohsin-Imran <mohsin.centosquare@gmail.com>
2024-12-26 18:47:02 +0500

    Code

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -15,26 +15,26 @@
     <div class="row">
         <div class="card p-2">
             <div class="card-header p-2 bg-primary d-flex flex-row justify-content-between">
-                <a href="{{ route('admin.category.create') }}" class="btn-sm btn-danger p-2 text-dark"><i class="fa fa-user-plus"></i> إضافة فئة</a>
+                <a href="{{ route('admin.category.create') }}" class="btn-sm btn-danger p-2 text-dark">إضافة فئة <i class="fa fa-user-plus"></i>  </a>
                 <h3 class="mb-0 text-white">قوائم الفئات</h3>
             </div>
             <div class="table-responsive mt-4">
                 <table id="example" class="tabel table-bordered  table-hover  table-striped">
                     <thead>
                         <tr>
-                            <th>الإجراء</th>
                             <th>الاسم</th>
+                            <th>الإجراءات</th>
                         </tr>
                     </thead>
                     <tbody>
                         @foreach ($categories as $data)
                         <tr>
 
+                            <td>{{ $data->name ?? 'N/A'}}</td>
                             <td class="p-1">
                                 <a href="{{ route('admin.category.edit',$data->id) }}" class="fa fa-pencil btn-sm btn-success" style="font-size: 15px; margin:5px;"></a>
                                 <a href="#" data-bs-toggle="modal" data-bs-target="#modalId{{ $data->id }}" class="fa fa-trash btn-sm btn-red ml-3" style="font-size: 15px; background-color: red; color: white;"></a>
                             </td>
-                            <td>{{ $data->name ?? 'N/A'}}</td>
                         </tr>
                         <div class="modal fade" id="modalId{{ $data->id }}" tabindex="-1" role="dialog" aria-labelledby="modalTitleId" aria-hidden="true">
                             <div class="modal-dialog" role="document">
